e56acb7dbe907e903c3e12a2824afe6f224d30be
aashish lohra <aashishkumarlohra9@gmail.com>
2024-12-04 00:11:56 +0530

    Learning Nextjs

---
+++ nextjs-dashboard/app/ui/fonts.ts
@@ -0,0 +1,14 @@
+import { Inter } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+
+
+// fonts.ts  
+import { Lusitana } from 'next/font/google';  
+
+const lusitana = Lusitana({  
+  weight: '400', // specify the font weight  
+  subsets: ['latin'], // specify the subset  
+});  
+
+export { lusitana };
